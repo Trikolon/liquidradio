fd5028cd99cf5cc5609127b0e8281375754a3c1e
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-07-01 01:12:52 +0200

    vue-twitter-feed: removed unused heading property

--- js/components.js
+++ js/components.js
@@ -1,4 +1,4 @@
 Vue.component("twitter-feed", {
-    template: "<a class=\"twitter-timeline\" :data-width=\"width\" :data-height=\"height\" data-dnt=\"true\" :data-theme=\"theme\" :href=\"profile\">{{heading}}</a>",
+    template: "<a class=\"twitter-timeline\" :data-width=\"width\" :data-height=\"height\" data-dnt=\"true\" :data-theme=\"theme\" :href=\"profile\"></a>",
     props: ["theme", "profile", "heading", "width", "height"]
 });
