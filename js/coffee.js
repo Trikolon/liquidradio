95ebf1495794166ed066944c05dab56ca93b2e5b
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-07-01 00:39:59 +0200

    redone layout to app material style, added notification bar

--- js/coffee.js
+++ js/coffee.js
@@ -2,17 +2,25 @@ Vue.use(VueMaterial);
 const app = new Vue({
     el: "#app",
     data: {
+        title: "CFP Radio",
         notSupportedMessage: "Your browser does not support audio streams, please update.",
         stream: {
             play: false,
             type: "audio/mpeg",
             src: "http://equinox.shoutca.st:8702/;stream/1",
+            title: "Bassdrive",
             el: "streamEl"
         },
         twitterFeed: {
             heading: "Tweets by @thecoffeepanda",
             profile: "https://twitter.com/thecoffeepanda",
             theme: "dark"
+        },
+        notification: {
+            message: "",
+            duration: 4000,
+            position: "top center",
+            el: "notification-bar"
         }
     },
     watch: {
@@ -29,11 +37,12 @@ const app = new Vue({
             const el = this.$refs[this.stream.el];
             console.debug("play state changed to", state);
             if (state) {
-                console.debug("started el");
+                console.debug("started stream");
+                this.notify("Now playing " + this.stream.title, 2000);
                 el.play();
             }
             else {
-                console.debug("stopped el");
+                console.debug("stopped stream");
                 el.pause();
             }
         },
@@ -49,6 +58,14 @@ const app = new Vue({
             else {
                 this.stream.play = true;
             }
+        },
+        notify: function (message, duration) {
+            const el = this.$refs[this.notification.el];
+            if (duration) {
+                this.notification.duration = duration;
+            }
+            this.notification.message = message;
+            el.open();
         }
     }
 });
