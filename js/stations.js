c3b025156b240e67a251a0ddaa28af36f9d06083
Trikolon <pbz@posteo.de>
2017-07-04 14:28:45 +0200

    Moved station data to separate file

---
+++ js/stations.js
@@ -0,0 +1,14 @@
+const stations = [
+    {
+        id: "liquid_radio",
+        type: "audio/mp3",
+        src: "http://proxima.shoutca.st:8368/;",
+        title: "Liquid Radio"
+    },
+    {
+        id: "bassdrive",
+        type: "audio/mpeg",
+        src: "http://equinox.shoutca.st:8702/;stream/1",
+        title: "Bassdrive"
+    }
+];
