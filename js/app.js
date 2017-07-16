Trikolon/liquidradio

commit 7bda307e6fc3913002c6cb6b12fb6139843eb62d

Refactored visualizer * Create AudioContext and attach mediaElementSource in parent * fixes #14

js/app.js
@@ -23,6 +23,8 @@ const app = new Vue({
             volume: parseFloat(localStorage ? localStorage.getItem("volume") || "0.6" : "0.6"),
             el: "streamEl",
             dom: undefined,
+            audioContext: undefined,
+            mediaElSrc: undefined,
             currentStation: undefined,
             stations
         },
@@ -66,9 +68,11 @@ const app = new Vue({
         }
     },
     mounted() {
-        this.stream.dom = this.$refs[this.stream.el];
+        this.stream.dom = this.$refs[this.stream.el]; // Get and save dom for further use
+        this.stream.audioContext = new AudioContext(); // Create audio context for visualizer
+        this.stream.mediaElSrc = this.stream.audioContext.createMediaElementSource(this.stream.dom); // for visualizer
 
-        // Watch external changes
+        // Attach event listeners to stream dom to watch external changes
         this.stream.dom.addEventListener("play", () => {
             this.stream.play = true;
             this.stream.loading = true;

js/visualizer.js
@@ -5,7 +5,8 @@ export default () => {
     Vue.component("audio-visualizer", {
         template: "<canvas></canvas>",
         props: {
-            audioid: {},
+            audioContext: {},
+            mediaElSrc: {},
             design: {
                 default: "square",
                 validator (value) {
@@ -36,60 +37,41 @@ export default () => {
             this.init();
         },
         beforeDestroy() {
-            //TODO: de-register components to fix error on visualizer toggle
-            //   this.audio.disconnect();
-            // this.ctx.close();
+            // TODO: Check if we need to do cleanup
         },
         methods: {
             init() {
-                if (this.interval) {
-                    log.debug("Interval active");
-                    this.audioel = document.getElementById(this.audioid);
-                    log.debug("element by id returned", this.audioid, this.audioel);
-                    if (this.audioel) {
-                        log.debug("audioel found by id");
-                        clearInterval(this.interval);
-                        this._init();
+                Vue.nextTick(() => {
+                    log.debug("Visualizer variables", this.audioContext, this.mediaElSrc);
+                    this.divider = 16; // data "resolution" divider
+
+                    this.analyser = this.audioContext.createAnalyser();
+                    this.analyser.fftSize = this.analyser.fftSize / this.divider;
+                    this.analyser.maxDecibels = this.maxdb;
+                    this.analyser.smoothingTimeConstant = 0.9;
+
+                    // connections to analyser and sound output
+                    this.mediaElSrc.connect(this.analyser);
+                    this.mediaElSrc.connect(this.audioContext.destination); // disable when testing
+
+                    // setup data array
+                    this.bufferLength = this.analyser.frequencyBinCount;
+                    this.freqBytes = new Uint8Array(this.bufferLength);
+                    this.waveBytes = new Uint8Array(this.bufferLength);
+
+                    // setup canvas and canvas context variables
+                    this.canvas = this.$el;
+                    this.c = this.canvas.getContext("2d");
+
+                    this.resize();
+
+                    if (this.design === "square") {
+                        requestAnimationFrame(this.draw);
                     }
-                }
-                else {
-                    log.debug("Interval inactive, starting");
-                    this.interval = setInterval(this.init, 100);
-                    this.init();
-                }
-            },
-            _init() {
-                this.divider = 16; // data "resolution" divider
-                // get audio element & build audio analyser
-                this.ctx = new window.AudioContext();
-                this.audio = this.ctx.createMediaElementSource(this.audioel);
-
-                this.analyser = this.ctx.createAnalyser();
-                this.analyser.fftSize = this.analyser.fftSize / this.divider;
-                this.analyser.maxDecibels = this.maxdb;
-                this.analyser.smoothingTimeConstant = 0.9;
-
-                // connections to analyser and sound output
-                this.audio.connect(this.analyser);
-                this.audio.connect(this.ctx.destination); // disable when testing
-
-                // setup data array
-                this.bufferLength = this.analyser.frequencyBinCount;
-                this.freqBytes = new Uint8Array(this.bufferLength);
-                this.waveBytes = new Uint8Array(this.bufferLength);
-
-                // setup canvas and canvas context variables
-                this.canvas = this.$el;
-                this.c = this.canvas.getContext("2d");
-
-                this.resize();
-
-                if (this.design === "square") {
-                    requestAnimationFrame(this.draw);
-                }
-                else if (this.design === "circle") {
-                    requestAnimationFrame(this.drawCircle);
-                }
+                    else if (this.design === "circle") {
+                        requestAnimationFrame(this.drawCircle);
+                    }
+                });
             },
             /**
              * recalculate values needed by draw functions, supposed to be called on each resize event
@@ -151,7 +133,7 @@ export default () => {
              */
             draw() {
                 // skip logic if audio is paused or canvas is hidden
-                if (this.audioel.paused || this.canvas.offsetHeight === 0) {
+                if (this.mediaElSrc.mediaElement.paused || this.canvas.offsetHeight === 0) {
                     requestAnimationFrame(this.draw);
                     return;
                 }
@@ -182,7 +164,7 @@ export default () => {
              */
             drawCircle() {
                 // skip logic if audio is paused or canvas is hidden
-                if (this.audioel.paused || this.canvas.offsetHeight === 0) {
+                if (this.mediaElSrc.mediaElement.paused || this.canvas.offsetHeight === 0) {
                     requestAnimationFrame(this.drawCircle);
                     return;
                 }

public/index.html
@@ -110,7 +110,8 @@
     <audio id="streamEl" ref="streamEl" preload="metadata" crossorigin="anonymous">
         <source v-for="source in stream.currentStation.source" :key="source.src" :src="source.src" :type="source.type">
     </audio>
-    <audio-visualizer v-if="visualizer" id="visualizer" design="square" :audioid="stream.el"
+    <audio-visualizer v-if="visualizer" id="visualizer" design="square" :audio-context="stream.audioContext"
+                      :media-el-src="stream.mediaElSrc"
                       barcolor="#E91E63"></audio-visualizer>
     <md-progress id="volumeBar" class="md-accent" :md-progress="stream.volume*100"
                  :md-indeterminate="stream.loading"></md-progress>

commit 9950a49294b6dff74b4b655c6c538d071e363bf7

Fixed bug introduced in 7bda307

js/app.js
@@ -71,6 +71,7 @@ const app = new Vue({
         this.stream.dom = this.$refs[this.stream.el]; // Get and save dom for further use
         this.stream.audioContext = new AudioContext(); // Create audio context for visualizer
         this.stream.mediaElSrc = this.stream.audioContext.createMediaElementSource(this.stream.dom); // for visualizer
+        this.stream.mediaElSrc.connect(this.stream.audioContext.destination); // connect so we have audio
 
         // Attach event listeners to stream dom to watch external changes
         this.stream.dom.addEventListener("play", () => {

js/visualizer.js
@@ -49,9 +49,8 @@ export default () => {
                     this.analyser.maxDecibels = this.maxdb;
                     this.analyser.smoothingTimeConstant = 0.9;
 
-                    // connections to analyser and sound output
+                    // connect to analyser
                     this.mediaElSrc.connect(this.analyser);
-                    this.mediaElSrc.connect(this.audioContext.destination); // disable when testing
 
                     // setup data array
                     this.bufferLength = this.analyser.frequencyBinCount;
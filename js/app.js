9950a49294b6dff74b4b655c6c538d071e363bf7
Paul Zuehlcke <1077860+Trikolon@users.noreply.github.com>
2017-07-16 23:10:40 +0200

    Fixed bug introduced in 7bda307

--- js/app.js
+++ js/app.js
@@ -71,6 +71,7 @@ const app = new Vue({
         this.stream.dom = this.$refs[this.stream.el]; // Get and save dom for further use
         this.stream.audioContext = new AudioContext(); // Create audio context for visualizer
         this.stream.mediaElSrc = this.stream.audioContext.createMediaElementSource(this.stream.dom); // for visualizer
+        this.stream.mediaElSrc.connect(this.stream.audioContext.destination); // connect so we have audio
 
         // Attach event listeners to stream dom to watch external changes
         this.stream.dom.addEventListener("play", () => {
