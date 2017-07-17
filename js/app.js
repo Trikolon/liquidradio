73b3074e0638e307ca4055c2d1c44c3efc8279e0
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-07-17 18:49:11 +0200

    Incremented version to 1.1 for dev

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const app = new Vue({
         loglevel: process.env.NODE_ENV === 'production' ? "INFO" : "DEBUG",
         title: "Liquid Radio",
         repoLink: "https://github.com/Trikolon/liquidradio",
-        version: "1.0",
+        version: "1.0.1",
         stream: {
             play: false,
             offline: false,
