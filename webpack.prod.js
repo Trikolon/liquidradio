05e6914580c4ed838526abd5fff442a964e4c9b5
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-08-01 21:44:15 +0200

    Transpile to es5 in production js build  * Added babel dependencies  * Updated webpack production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,6 +16,16 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'es2015']
+                    }
+                }
             }
         ]
     },
