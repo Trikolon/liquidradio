6a6c20a7066ee7bd022e3883c139aab643aacc14
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-11-20 23:09:29 +0100

    Added sentry.io error reporting; Updated dependencies

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import VueRouter from 'vue-router';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
 
 import App from './components/App.vue';
 
 window.log = loglevel.getLogger('liquidradio'); // Get a custom logger to prevent webpack-dev-server from controlling it
 
 if (process.env.NODE_ENV === 'production') {
+  Raven
+    .config('https://81882876703347e89790f8c3cc71362e@sentry.io/248115')
+    .addPlugin(RavenVue, Vue)
+    .install();
   log.setDefaultLevel('INFO');
   OfflinePluginRuntime.install();
 } else {
