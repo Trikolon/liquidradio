be8e5bfab3fdb3f30000770c0534fbae20530a12
Trikolon <1077860+Trikolon@users.noreply.github.com>
2018-01-29 22:18:51 +0100

    Disabled sourcemap in production as it was causing build errors

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV === 'production') { // only add in production
-  module.exports.devtool = '#source-map';
+  // module.exports.devtool = '#source-map'; // FIXME
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
